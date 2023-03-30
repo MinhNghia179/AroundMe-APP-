dcb6568db424af304558423c8c6671c49e175258
Minh Nghia <nguyenminhnghia.thd@gmail.com>
2023-03-30 14:23:29 +0700

    update

--- src/infrastructure/theme/index.ts
+++ src/infrastructure/theme/index.ts
@@ -1,1 +1,14 @@
-export const themes = {};
+import { colors } from './colors';
+import { fonts, fontSizes, fontWeights } from './fonts';
+import { sizes, space } from './tokens';
+
+export const theme = {
+  colors,
+  sizes,
+  space,
+  fonts,
+  fontSizes,
+  fontWeights,
+};
+
+export type Theme = typeof theme;

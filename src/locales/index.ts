64e87badd3bf065935be9da32cdcf00971c784c9
Minh Nghia <nguyenminhnghia.thd@gmail.com>
2023-06-15 16:42:22 +0700

    Update

---
+++ src/locales/index.ts
@@ -0,0 +1,4 @@
+import { messagesEn as MESSAGES_EN } from './messages/message.en';
+import { messagesVn as MESSAGES_VN } from './messages/message.vn';
+
+export default { MESSAGES_EN, MESSAGES_VN };

c3cba222b7309be8b8ea27d0ca3b46a3f1a547d8
Minh Nghia <nguyenminhnghia.thd@gmail.com>
2023-06-27 16:36:50 +0700

    example toggle color button

--- src/screens/home/HomePageView.tsx
+++ src/screens/home/HomePageView.tsx
@@ -1,11 +1,14 @@
 import { Text } from '@app/components/design -system';
+import useTheme from '@app/hooks/useTheme';
 import React from 'react';
-import { View } from 'react-native';
+import { Button, View } from 'react-native';
 
 const HomePageView = () => {
+  const { toggle } = useTheme();
   return (
     <View style={{ flex: 1 }}>
       <Text>HomePageView</Text>
+      <Button onPress={toggle} title="Toggle Color" />
     </View>
   );
 };
